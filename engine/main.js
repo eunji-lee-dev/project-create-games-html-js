930417dbc1c8c142f9235ef2f8bc9341c654132a
eunji-lee-dev <eunji.lee.dev@gmail.com>
2020-12-11 14:52:13 +0100

    Add: main.js

--- engine/main.js
+++ engine/main.js
@@ -5,10 +5,37 @@ const  playTime = document.querySelector(".play-time");
 
 const tileCount = 16;
 
-Array(tileCount).fill().forEach((_, i)=> {
-	const li = document.createElement("li");
-	li.setAttribute('data-index', i);
-	li.classList.add(`list${i}`);
-	console.log(li);
-	container.appendChild(li);
-});
+let tiles = [];
+
+SetGame();
+
+function SetGame() {
+	tiles = createImgTiles();
+	tiles.forEach(tile => container.appendChild(tile));
+	setTimeout(() => {
+		container.innerHTML = "";
+		mix(tiles).forEach(tile => container.appendChild(tile))
+	},2000);
+}
+
+function createImgTiles() {
+	const tempArray = [];
+	Array(tileCount).fill().forEach((_, i)=> {
+		const li = document.createElement("li");
+		li.setAttribute('data-index', i);
+		li.classList.add(`list${i}`);
+		console.log(li);
+		tempArray.push(li);
+	})
+	return tempArray;
+}
+
+function mix(array) {
+	let index = array.length - 1;
+	while (index > 0) {
+		const  randomIndex = Math.floor(Math.random() * (index + 1));
+		[array[index], array[randomIndex]] = [array[randomIndex], array[index]]
+		index--;
+	}
+	return array;
+}
